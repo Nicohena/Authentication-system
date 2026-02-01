2ec1d7dbc0344d3054c0118005f8b83fdeb3503f
henok <nicohena19@gmail.com>
2026-02-01 12:01:10 +0300

    home page with navbar and header and routing setup

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../componenets/Navbar.jsx'
+import Header from '../componenets/Header.jsx'
 const Home = () => {
   return (
-    <div>
-        <h1>Home Page</h1>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
+      <Navbar/>
+      <Header/>
     </div>
   )
 }
